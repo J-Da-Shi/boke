50f2336ee4189b09151d7333a5e34371586a5a1c
shida <775734947@qq.com>
2026-05-16 13:58:32 +0800

    feature: 我的博客

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/boke/',
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
   },
 })
